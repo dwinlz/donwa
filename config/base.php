15b126932a31406606a89a76f2a3f16e5f22efc4
dwinlz <48846246+dwinlz@users.noreply.github.com>
2019-03-23 10:20:02 +0800

    Add files via upload

---
+++ config/base.php
@@ -0,0 +1,106 @@
+<?php return array (
+  'site_name' => 'private space',
+  'password' => 'gqsai123@',
+  'style' => 'material',
+  'onedrive_root' => '/',
+  'cache_type' => 'secache',
+  'cache_expire_time' => 3600,
+  'cache_refresh_time' => 600,
+  'root_path' => '?',
+  'show' => 
+  array (
+    'stream' => 
+    array (
+      0 => 'txt',
+    ),
+    'image' => 
+    array (
+      0 => 'bmp',
+      1 => 'jpg',
+      2 => 'jpeg',
+      3 => 'png',
+      4 => 'gif',
+    ),
+    'video5' => 
+    array (
+      0 => 'mp4',
+      1 => 'webm',
+      2 => 'mkv',
+    ),
+    'video' => 
+    array (
+    ),
+    'video2' => 
+    array (
+      0 => 'avi',
+      1 => 'mpg',
+      2 => 'mpeg',
+      3 => 'rm',
+      4 => 'rmvb',
+      5 => 'mov',
+      6 => 'wmv',
+      7 => 'asf',
+      8 => 'ts',
+      9 => 'flv',
+    ),
+    'audio' => 
+    array (
+      0 => 'ogg',
+      1 => 'mp3',
+      2 => 'wav',
+    ),
+    'code' => 
+    array (
+      0 => 'html',
+      1 => 'htm',
+      2 => 'php',
+      3 => 'css',
+      4 => 'go',
+      5 => 'java',
+      6 => 'js',
+      7 => 'json',
+      8 => 'txt',
+      9 => 'sh',
+      10 => 'md',
+    ),
+    'doc' => 
+    array (
+      0 => 'csv',
+      1 => 'doc',
+      2 => 'docx',
+      3 => 'odp',
+      4 => 'ods',
+      5 => 'odt',
+      6 => 'pot',
+      7 => 'potm',
+      8 => 'potx',
+      9 => 'pps',
+      10 => 'ppsx',
+      11 => 'ppsxm',
+      12 => 'ppt',
+      13 => 'pptm',
+      14 => 'pptx',
+      15 => 'rtf',
+      16 => 'xls',
+      17 => 'xlsx',
+    ),
+  ),
+  'images' => 
+  array (
+    'home' => false,
+    'public' => false,
+    'exts' => 
+    array (
+      0 => 'jpg',
+      1 => 'png',
+      2 => 'gif',
+      3 => 'bmp',
+    ),
+  ),
+  'client_secret' => 'chnexPBXON808_[|auSY60!',
+  'client_id' => 'chnexPBXON808_[|auSY60!',
+  'redirect_uri' => 'https://hkoneindex.herokuapp.com/',
+  'refresh_token' => 'OAQABAAAAAACEfexXxjamQb3OeGQ4GugvpNxdmLj9yvEeJSYoLRtBzN53E4lGCGYVKgGJ04pZVkh6dRo9oMomtIhZLqcspAKGi1IVmucKFdBR79usd9bZKGBv3yP2zQIIASZSAQSbyWcfHj-KpH5d1Jev5QGDMA14punWZGk5FasXbN18R-8egM8xGFpjuGdktVcpmFWaShe6md_XNh6E-B8nFEjUIvWnwWiEsqB16T9BBYuEYKbm75kQMikwVbgCSbHk7y7nl-PlrEzbMtYWIAJFWAHkKCingO6vo2F3Mz78MZcUSjHIUr9_R5O_xTpSoyW3-Poqi4kYsZy9xHB9RHnWb44Slu3fx7QGe-X9CQqLqwEoQ8esgeBoZ4DiRa5csMRjltU9Jkgh6ugKSlWAc8a31dsroF-w-61kb-9LAA5LhYvpl89EVbckUT57BhqrJxiAx__D132_9QrDJZFIrGgjECvVaNrqGGWfAcxfA5Zh2HTCBaCktzGticwtfzpAMZuInON0UzK2--KBdf4im6EShOI3IMbELcR0l-vF32vmJ5rivwY__UYbEVxqJnfTpyEWCkXN-ZYc49yNij5m0LLrUVG8yLltLnUVLAjbmHLKaO7EeSZCsbJdBRC6jtIX6XuGP5fW1hDqdJdjPXkWuiRMqwZphGQC9l7bYO-4M-tDohAsxSBZv42X6hDTU5EQIoovZRDa5dFuQGzUd0fiEaDH7geIDywFh7IXOQp2ONfKdgrYDvFZl3W38lUoeQIgAZM_hl6JfquTv1E14xl3voFJ6pc1oJo04FRI6DTZaEa-Rlqoai6kFSAA',
+  'onedrive_hide' => '',
+  'onedrive_hotlink' => '',
+);
